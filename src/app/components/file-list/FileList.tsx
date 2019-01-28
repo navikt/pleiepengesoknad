8ad8d34accf25fe59b2d65435ce79368706e838c
Erlend Vige <erlend.vige@gmail.com>
2019-01-28 10:08:37 +0100

    Attachment-icon
    Adds attachment icons to FileList elements.

--- src/app/components/file-list/FileList.tsx
+++ src/app/components/file-list/FileList.tsx
@@ -1,18 +1,27 @@
 import * as React from 'react';
 import DeleteButton from '../delete-button/DeleteButton';
 import './fileList.less';
+import CustomSVG from '../custom-svg/CustomSVG';
+import bemHelper from '../../utils/bemHelper';
+const attachmentIcon = require('./attachment.svg').default;
 
 interface FileListProps {
     files: File[];
     onRemoveFileClick: (file: File, e: React.SyntheticEvent) => void;
 }
 
+const listBem = bemHelper('fileList');
+const listElementBem = bemHelper(`${listBem.className}__element`);
+
 const FileList: React.FunctionComponent<FileListProps> = ({ files, onRemoveFileClick }) => (
-    <ul className="fileList">
+    <ul className={listBem.className}>
         {files.map((file, index) => (
-            <li key={file.name + index}>
+            <li className={listElementBem.className} key={file.name + index}>
                 <span>
-                    {file.name} ({file.size})
+                    <CustomSVG iconRef={attachmentIcon} size={22} />
+                    <div className={listElementBem.element('text')}>
+                        {file.name} ({file.size})
+                    </div>
                 </span>
                 <DeleteButton ariaLabel="Slett vedlegg" onClick={(e) => onRemoveFileClick(file, e)}>
                     Fjern fil
