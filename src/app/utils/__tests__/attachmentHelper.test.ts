fd44a1a0d904097cc16357add766646a99d77aa7
Erlend Vige <erlend.vige@gmail.com>
2019-01-30 15:07:03 +0100

    attachmentHelper tests

---
+++ src/app/utils/__tests__/attachmentHelper.test.ts
@@ -0,0 +1,23 @@
+import { fileExtensionIsValid, getAttachmentFromFile, VALID_EXTENSIONS } from '../attachmentHelper';
+
+describe('attachmentHelper', () => {
+    describe('fileExtensionIsValid', () => {
+        it('should only accept file extensions specified by VALID_EXTENSIONS', () => {
+            for (const ext of VALID_EXTENSIONS) {
+                expect(fileExtensionIsValid(ext)).toBe(true);
+            }
+            expect(fileExtensionIsValid('asdf')).toBe(false);
+        });
+    });
+
+    describe('getAttachmentFromFile', () => {
+        it('should return an Attachment-object containing specified File and Attachment-specific properties', () => {
+            const fileMock = new File([''], 'filename', { type: 'text/png' });
+            const { pending, file, uploaded, url } = getAttachmentFromFile(fileMock);
+            expect(file).toEqual(fileMock);
+            expect(pending).toBe(false);
+            expect(uploaded).toBe(false);
+            expect(url).toBeUndefined();
+        });
+    });
+});
