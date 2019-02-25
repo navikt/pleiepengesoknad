8d267654cc385d39e98865dc0cac3955bc0504b8
Erlend Vige <erlend.vige@gmail.com>
2019-02-25 14:35:26 +0100

    søkerdataUtils tests

---
+++ src/app/utils/__tests__/søkerdataUtils.ts
@@ -0,0 +1,22 @@
+import { harRegistrerteBarn } from '../søkerdataUtils';
+import { Barn, Søkerdata } from '../../types/Søkerdata';
+
+const barnMock: Partial<Barn> = {
+    fornavn: 'Ola'
+};
+
+describe('søkerdataUtils', () => {
+    describe('harRegistrerteBarn', () => {
+        it('should return true if barn-array length his greater than 0', () => {
+            const søkerdata: Partial<Søkerdata> = { barn: [barnMock as Barn] };
+            expect(harRegistrerteBarn(søkerdata as Søkerdata)).toBe(true);
+        });
+
+        it('should return false if barn array is missing or length is 0', () => {
+            const søkerdata: Partial<Søkerdata> = { barn: undefined };
+            expect(harRegistrerteBarn(søkerdata as Søkerdata)).toBeFalsy();
+            søkerdata.barn = [];
+            expect(harRegistrerteBarn(søkerdata as Søkerdata)).toBeFalsy();
+        });
+    });
+});
