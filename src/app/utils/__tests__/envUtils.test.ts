1dac10e981d74645db0397e9247ff85dd9fdf777
Erlend Vige <erlend.vige@gmail.com>
2019-02-25 13:18:26 +0100

    appIsRunningInDevEnvironment test

--- src/app/utils/__tests__/envUtils.test.ts
+++ src/app/utils/__tests__/envUtils.test.ts
@@ -1,4 +1,4 @@
-import { getEnvironmentVariable } from '../envUtils';
+import { appIsRunningInDevEnvironment, getEnvironmentVariable } from '../envUtils';
 
 describe('envUtils', () => {
     describe('getEnvironmentVariable', () => {
@@ -6,6 +6,20 @@ describe('envUtils', () => {
             (window as any).appSettings = { someEnvVar: 1, someOtherEnvVar: 2 };
             expect(getEnvironmentVariable('someEnvVar')).toBe(1);
             expect(getEnvironmentVariable('someOtherEnvVar')).toBe(2);
+        });
+    });
+
+    describe('appIsRunningInDevEnvironment', () => {
+        it('should return true if process.env.NODE_ENV === "development"', () => {
+            (process as any).env = { NODE_ENV: 'development' };
+            expect(appIsRunningInDevEnvironment()).toBe(true);
+        });
+
+        it('should return false if process.env.NODE_ENV is set to something else than "development"', () => {
+            (process as any).env = { NODE_ENV: undefined };
+            expect(appIsRunningInDevEnvironment()).toBe(false);
+            (process as any).env = { NODE_ENV: 'production' };
+            expect(appIsRunningInDevEnvironment()).toBe(false);
         });
     });
 });
