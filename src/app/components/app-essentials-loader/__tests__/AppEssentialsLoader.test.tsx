5113780d6d44c809a0e0cb4d982aabbea3a3708f
Erlend Vige <erlend.vige@gmail.com>
2019-03-01 11:20:32 +0100

    <AppEssentialsLoader /> tests
    Add missing tests for making sure correct redirects are done when errors occur

--- src/app/components/app-essentials-loader/__tests__/AppEssentialsLoader.test.tsx
+++ src/app/components/app-essentials-loader/__tests__/AppEssentialsLoader.test.tsx
@@ -1,11 +1,14 @@
 import * as React from 'react';
-import { cleanup, render, waitForElement } from 'react-testing-library';
+import { cleanup, render, waitForElement, wait } from 'react-testing-library';
 import AppEssentialsLoader from '../AppEssentialsLoader';
 import * as api from './../../../api/api';
+import * as apiUtils from './../../../utils/apiUtils';
+import routeConfig from '../../../config/routeConfig';
+import Mock = jest.Mock;
 
 jest.mock('./../../../utils/envUtils', () => {
     return {
-        getEnvironmentVariable: () => 'someEnvVarValue'
+        getEnvironmentVariable: () => 'someEnvVar'
     };
 });
 
@@ -13,6 +16,13 @@ jest.mock('./../../../api/api', () => {
     return {
         getBarn: jest.fn().mockResolvedValue('barndata'),
         getSøker: jest.fn().mockResolvedValue('søkerdata')
+    };
+});
+
+jest.mock('./../../../utils/apiUtils', () => {
+    return {
+        isForbidden: jest.fn(() => false),
+        isUnauthorized: jest.fn(() => false)
     };
 });
 
@@ -36,6 +46,28 @@ describe('<AppEssentialsLoader />', () => {
         await waitForElement(() => getByText(stringContent));
         expect(getByText(stringContent)).toBeTruthy();
         expect(queryByTestId('spinner-element')).toBeNull();
+    });
+
+    it('should redirect the user to an error page if getBarn or getSøker failed, but user is logged in', async () => {
+        (api.getBarn as any) = jest.fn().mockRejectedValue('reject');
+        render(<AppEssentialsLoader contentLoadedRenderer={() => null} />);
+        await wait();
+        expect(window.location.assign).toHaveBeenCalledWith(routeConfig.ERROR_PAGE_ROUTE);
+        (api.getSøker as any) = jest.fn().mockRejectedValue('reject');
+        render(<AppEssentialsLoader contentLoadedRenderer={() => null} />);
+        await wait();
+        expect(window.location.assign).toHaveBeenCalledWith(routeConfig.ERROR_PAGE_ROUTE);
+    });
+
+    it('should redirect the user to LOGIN_URL if getBarn or getSøker responds with forbidden or unauthorized', async () => {
+        (apiUtils.isForbidden as Mock).mockImplementationOnce(() => true);
+        render(<AppEssentialsLoader contentLoadedRenderer={() => null} />);
+        await wait();
+        expect(window.location.assign).toHaveBeenCalledWith('someEnvVar');
+        (apiUtils.isUnauthorized as Mock).mockImplementationOnce(() => true);
+        render(<AppEssentialsLoader contentLoadedRenderer={() => null} />);
+        await wait();
+        expect(window.location.assign).toHaveBeenCalledWith('someEnvVar');
     });
 
     afterAll(cleanup);
