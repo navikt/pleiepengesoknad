e1737bb3ceaffc27b4044fa6c3535e40d0c0174c
Erlend Vige <erlend.vige@gmail.com>
2019-03-26 09:41:46 +0100

    Setup travis for pleiepengesoknad-mock

--- api-mock.js
+++ api-mock.js
@@ -4,7 +4,7 @@ const Busboy = require('busboy');
 const server = express();
 
 server.use((req, res, next) => {
-    const allowedOrigins = ['https://pleiepengesoknad-web.herokuapp.com', 'http://localhost:8080'];
+    const allowedOrigins = ['https://pleiepengesoknad-mock.nais.oera.no', 'http://localhost:8080'];
     const requestOrigin = req.headers.origin;
     if (allowedOrigins.indexOf(requestOrigin) >= 0) {
         res.set('Access-Control-Allow-Origin', requestOrigin);
