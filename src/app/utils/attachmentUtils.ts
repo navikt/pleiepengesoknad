fc78079f74b0a65b6fcb5490b78113012732d08d
Erlend Vige <erlend.vige@gmail.com>
2019-04-03 12:50:50 +0200

    VALID_EXTENSIONS ignoreCase
    Ensure we ignore casing when checking if file extensions are valid

--- src/app/utils/attachmentUtils.ts
+++ src/app/utils/attachmentUtils.ts
@@ -2,7 +2,7 @@ export const VALID_EXTENSIONS = ['.pdf', '.jpeg', '.jpg', '.png'];
 
 export const fileExtensionIsValid = (filename: string): boolean => {
     const ext = filename.split('.').pop();
-    return VALID_EXTENSIONS.includes(`.${ext!}`);
+    return VALID_EXTENSIONS.includes(`.${ext!.toLowerCase()}`);
 };
 
 export const getAttachmentFromFile = (file: File): Attachment => ({
