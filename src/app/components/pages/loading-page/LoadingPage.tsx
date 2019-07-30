d17e83d5070357377636879ce82055020587de33
Frode Hansen <frode.hansen@nav.no>
2019-07-30 10:31:12 +0200

    Remove intl from LoadingPage

--- src/app/components/pages/loading-page/LoadingPage.tsx
+++ src/app/components/pages/loading-page/LoadingPage.tsx
@@ -1,13 +1,11 @@
 import * as React from 'react';
 import Page from '../../page/Page';
 import LoadingSpinner from '../../loading-spinner/LoadingSpinner';
-import intlHelper from 'app/utils/intlUtils';
-import { injectIntl, InjectedIntlProps } from 'react-intl';
 
-const LoadingPage: React.FunctionComponent<InjectedIntlProps> = ({ intl }) => (
-    <Page title={intlHelper(intl, 'page.loadingPage.sidetittel')}>
+const LoadingPage: React.FunctionComponent = () => (
+    <Page title="Laster...">
         <LoadingSpinner type="XXL" />
     </Page>
 );
 
-export default injectIntl(LoadingPage);
+export default LoadingPage;
