ca6382f94967a993a3b5e76477b9337d21d674e3
Frode Hansen <frode.hansen@nav.no>
2019-08-06 14:08:17 +0200

    Add intl to stepConfig

--- src/app/components/step/__tests__/Step.test.tsx
+++ src/app/components/step/__tests__/Step.test.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import Step from '../Step';
 import { render, RenderResult } from 'react-testing-library';
-import { stepConfig, StepID } from '../../../config/stepConfig';
+import { StepID } from '../../../config/stepConfig';
 import { MemoryRouter } from 'react-router';
 import MockIntlProvider from '../../intl-provider/MockIntlProvider';
 
@@ -11,7 +11,12 @@ const renderWrappedInMemoryRouter = (child: React.ReactNode) =>
             <MemoryRouter>{child}</MemoryRouter>
         </MockIntlProvider>
     );
+
 const handleSubmit = jest.fn();
+
+export const intlForTests = {
+    formatMessage: jest.fn((a: any, b: any) => a + b)
+};
 
 describe('<Step>', () => {
     const stepID: StepID = StepID.OPPLYSNINGER_OM_BARNET;
@@ -25,14 +30,8 @@ describe('<Step>', () => {
     it('should render common <Step> content', () => {
         const { getByText } = renderResult;
         expect(getByText('Søknad om pleiepenger')).toBeTruthy();
-        expect(getByText(stepConfig[stepID].stepTitle)).toBeTruthy();
-        expect(getByText(stepConfig[stepID].buttonLabel!)).toBeTruthy();
+        expect(getByText('Barn')).toBeTruthy();
+        expect(getByText('Fortsett')).toBeTruthy();
         expect(getByText('Tilbake')).toBeTruthy();
     });
-
-    // it('should call handleSubmit-callback when submit-button is clicked', () => {
-    //    (window as any).HTMLFormElement.prototype.submit = () => {};
-    //    fireEvent.click(renderResult.container.querySelector('button[type="submit"]')!);
-    //    expect(handleSubmit).toHaveBeenCalled();
-    // });
 });
