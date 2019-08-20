c9f3b2c22e75281571136514718c3d395d165d6f
Frode Hansen <frode.hansen@nav.no>
2019-08-20 09:11:26 +0200

    Bugfix - timeUtils

--- src/app/utils/timeUtils.ts
+++ src/app/utils/timeUtils.ts
@@ -1,4 +1,5 @@
 import { HoursAndMinutes } from 'app/types/HoursAndMinutes';
+import { Time } from 'app/components/time-input-base/TimeInputBase';
 
 export const convertHoursToIso8601Duration = (time: number): string => {
     const { hours, minutes } = convertHoursToHoursAndMinutes(time);
@@ -12,4 +13,19 @@ export const convertHoursToHoursAndMinutes = (time: number): HoursAndMinutes => 
         hours,
         minutes
     };
+};
+
+export const getDecimalTimeFromTime = (time: Time): number => {
+    return (time.hours || 0) + ((100 / 60) * (time.minutes || 0)) / 100;
+};
+
+export const isValidTime = (time: Partial<Time> | undefined): time is Time => {
+    return (
+        time !== undefined &&
+        time.hours !== undefined &&
+        !isNaN(time.hours) &&
+        time.minutes !== undefined &&
+        !isNaN(time.minutes) &&
+        time.minutes < 60
+    );
 };
