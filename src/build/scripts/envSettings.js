73e6d9dadbfbf62e99159a6b418d897bfe753165
Frode Hansen <frode.hansen@nav.no>
2019-08-22 15:13:42 +0200

    Feature for dagerMedPleie

--- src/build/scripts/envSettings.js
+++ src/build/scripts/envSettings.js
@@ -10,6 +10,7 @@ function createEnvSettingsFile(settingsFile) {
                 HENT_BARN_FEATURE: '${process.env.HENT_BARN_FEATURE}',
                 TOGGLE_LANGUAGE: '${process.env.TOGGLE_LANGUAGE}',
                 TOGGLE_GRADERT_ARBEID: '${process.env.TOGGLE_GRADERT_ARBEID}',
+                TOGGLE_DAGER_MED_PLEIE: '${process.env.TOGGLE_DAGER_MED_PLEIE}',
             };`
         );
     });
