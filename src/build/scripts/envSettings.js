9cb0f95fed935f39a4382629975b611c07044947
Frode Hansen <frode.hansen@nav.no>
2019-08-30 09:35:43 +0200

    Remove obsolete FEATURE_TOGGLEs

--- src/build/scripts/envSettings.js
+++ src/build/scripts/envSettings.js
@@ -7,8 +7,6 @@ function createEnvSettingsFile(settingsFile) {
             `window.appSettings = {
                 API_URL: '${process.env.API_URL}',
                 LOGIN_URL: '${process.env.LOGIN_URL}',
-                HENT_BARN_FEATURE: '${process.env.HENT_BARN_FEATURE}',
-                TOGGLE_LANGUAGE: '${process.env.TOGGLE_LANGUAGE}',
                 TOGGLE_FJERN_GRAD: '${process.env.TOGGLE_FJERN_GRAD}',
             };`
         );
