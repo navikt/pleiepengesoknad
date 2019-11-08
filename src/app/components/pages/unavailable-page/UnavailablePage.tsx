76783a278c0c4a6723384e56982754d99b55b40d
Frode Hansen <frodehansen@Frodes-MacBook-Pro.local>
2019-11-08 15:37:13 +0100

    Update

--- src/app/components/pages/unavailable-page/UnavailablePage.tsx
+++ src/app/components/pages/unavailable-page/UnavailablePage.tsx
@@ -4,25 +4,20 @@ import bemUtils from '../../../utils/bemUtils';
 import Box from '../../box/Box';
 import StepBanner from '../../step-banner/StepBanner';
 import { AlertStripeAdvarsel } from 'nav-frontend-alertstriper';
-import { Undertittel } from 'nav-frontend-typografi';
 import './unavailablePage.less';
 import Lenke from 'nav-frontend-lenker';
-import { injectIntl, InjectedIntlProps } from 'react-intl';
 
 const bem = bemUtils('introPage');
 
 const link =
     'https://www.nav.no/no/Person/Skjemaer-for-privatpersoner/skjemaveileder/vedlegg?key=333802&languagecode=53&veiledertype=privatperson';
 
-const UnavailablePage: React.StatelessComponent<InjectedIntlProps> = ({ intl }) => {
+const UnavailablePage: React.StatelessComponent<{}> = () => {
     const title = 'Søknad om pleiepenger';
     return (
         <Page className={bem.block} title={title} topContentRenderer={() => <StepBanner text={title} />}>
             <Box margin="xxxl">
                 <AlertStripeAdvarsel>
-                    <Box padBottom="s">
-                        <Undertittel>Pleiepengesøknaden er ikke tilgjengelig akkurat nå</Undertittel>
-                    </Box>
                     <p>
                         Den digitale pleiepengesøknaden er dessverre ikke tilgjengelig på grunn av teknisk feil. Vi
                         jobber med å løse feilen slik at du kan søke digitalt. Frem til vi får fikset dette, kan du
@@ -39,4 +34,4 @@ const UnavailablePage: React.StatelessComponent<InjectedIntlProps> = ({ intl }) 
     );
 };
 
-export default injectIntl(UnavailablePage);
+export default UnavailablePage;
