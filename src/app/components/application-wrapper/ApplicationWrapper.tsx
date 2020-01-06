29d078e37e9c53bfa7b6454f487cc314dc77878a
Frode Hansen <frode.hansen@bekk.no>
2020-01-06 10:42:42 +0100

    Update react-intl

--- src/app/components/application-wrapper/ApplicationWrapper.tsx
+++ src/app/components/application-wrapper/ApplicationWrapper.tsx
@@ -2,11 +2,11 @@ import * as React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import IntlProvider from '../intl-provider/IntlProvider';
 import { Søkerdata } from '../../types/Søkerdata';
-// import LanguageToggle from '../language-toggle/LanguageToggle';
 import { Locale } from 'common/types/Locale';
 import { Normaltekst } from 'nav-frontend-typografi';
 import DemoModeInfo from '../demo-mode-info/DemoModeInfo';
 import { appIsRunningInDemoMode } from '../../utils/envUtils';
+import LanguageToggle from 'common/components/language-toggle/LanguageToggle';
 
 interface ApplicationWrapperProps {
     søkerdata?: Søkerdata;
@@ -21,11 +21,8 @@ const ApplicationWrapper: React.FunctionComponent<ApplicationWrapperProps> = ({ 
         <IntlProvider locale={locale}>
             <Normaltekst tag="div">
                 {demoMode && <DemoModeInfo />}
-                {/* 
-                I Påvente av oversettelser
-                {demoMode === false && showLanguageToggle === true && (
-                    <LanguageToggle locale={locale} toggle={onChangeLocale} />
-                )} */}
+
+                {demoMode === false && <LanguageToggle locale={locale} toggle={onChangeLocale} />}
                 <Router>{children}</Router>
             </Normaltekst>
         </IntlProvider>
