2ba3902f7496983acad990524df36bbdf2bf0abf
Frode Hansen <frode.hansen@bekk.no>
2020-01-10 09:06:10 +0100

    Extend LoadingSpinner

--- src/common/components/loading-spinner/LoadingSpinner.tsx
+++ src/common/components/loading-spinner/LoadingSpinner.tsx
@@ -1,8 +1,34 @@
 import * as React from 'react';
 import Spinner, { NavFrontendSpinnerBaseProps as LoadingSpinnerProps } from 'nav-frontend-spinner';
+import { Element } from 'nav-frontend-typografi';
 
-const LoadingSpinner: React.FunctionComponent<LoadingSpinnerProps> = ({ type }) => (
-    <Spinner type={type} data-testid="spinner-element" />
-);
+interface OwnProps {
+    style?: 'inline' | 'block';
+    blockTitle?: string;
+}
+
+const LoadingSpinner: React.FunctionComponent<OwnProps & LoadingSpinnerProps> = ({
+    type,
+    style = 'inline',
+    blockTitle
+}) => {
+    const spinner = <Spinner type={type} data-testid="spinner-element" />;
+    if (style === 'inline') {
+        return spinner;
+    }
+    return (
+        <div
+            style={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                minHeight: '15rem',
+                alignItems: 'center'
+            }}>
+            <LoadingSpinner type="XXL" />
+            {blockTitle && <Element style={{ marginTop: '1rem' }}>{blockTitle}</Element>}
+        </div>
+    );
+};
 
 export default LoadingSpinner;
