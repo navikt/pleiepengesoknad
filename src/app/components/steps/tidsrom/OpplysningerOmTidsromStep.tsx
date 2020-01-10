5672eee845da712f5fed3cc0d1c048f29bfed459
Frode Hansen <frode.hansen@bekk.no>
2020-01-10 10:52:36 +0100

    Cleanup tidsromStep

--- src/app/components/steps/tidsrom/OpplysningerOmTidsromStep.tsx
+++ src/app/components/steps/tidsrom/OpplysningerOmTidsromStep.tsx
@@ -5,27 +5,16 @@ import { navigateTo } from '../../../utils/navigationUtils';
 import { AppFormField } from '../../../types/PleiepengesøknadFormData';
 import FormikStep from '../../formik-step/FormikStep';
 import DateIntervalPicker from '../../date-interval-picker/DateIntervalPicker';
-// import { SøkerdataContextConsumer } from '../../../context/SøkerdataContext';
-// import { Søkerdata } from '../../../types/Søkerdata';
 import { date3YearsAgo } from 'common/utils/dateUtils';
-// import { getArbeidsgiver } from '../../../api/api';
 import { validateYesOrNoIsAnswered, validateFradato, validateTildato } from '../../../validation/fieldValidations';
 import YesOrNoQuestion from '../../yes-or-no-question/YesOrNoQuestion';
 import Box from 'common/components/box/Box';
-// import { AxiosError } from 'axios';
-// import * as apiUtils from '../../../utils/apiUtils';
 import intlHelper from 'common/utils/intlUtils';
 import { InjectedIntlProps, injectIntl } from 'react-intl';
 import { YesOrNo } from 'common/types/YesOrNo';
 import { CustomFormikProps } from '../../../types/FormikProps';
-// import demoSøkerdata from '../../../demo/demoData';
-// import { appIsRunningInDemoMode } from '../../../utils/envUtils';
 
 import './dagerPerUkeBorteFraJobb.less';
-
-// interface OpplysningerOmTidsromStepState {
-//     isLoadingNextStep: boolean;
-// }
 
 interface OpplysningerOmTidsromStepProps {
     formikProps: CustomFormikProps;
@@ -33,77 +22,7 @@ interface OpplysningerOmTidsromStepProps {
 
 type Props = OpplysningerOmTidsromStepProps & HistoryProps & InjectedIntlProps & StepConfigProps;
 
-// type Props = CommonStepFormikProps & HistoryProps & InjectedIntlProps & StepConfigProps;
-
 const OpplysningerOmTidsromStep = ({ history, intl, nextStepRoute, formikProps, ...stepProps }: Props) => {
-    // const navigate = nextStepRoute ? () => navigateTo(nextStepRoute, history) : undefined;
-
-    // class OpplysningerOmTidsromStep extends React.Component<Props, OpplysningerOmTidsromStepState> {
-    //     constructor(props: Props) {
-    //         super(props);
-
-    //         this.getArbeidsforhold = this.getArbeidsforhold.bind(this);
-    //         this.finishStep = this.finishStep.bind(this);
-    //         this.validateFraDato = this.validateFraDato.bind(this);
-    //         this.validateTilDato = this.validateTilDato.bind(this);
-
-    //         // this.state = {
-    //         //     isLoadingNextStep: false
-    //         // };
-    //     }
-
-    // getArbeidsforhold() {
-    //     const values = this.props.formikProps.values;
-    //     const fromDateString = formatDateToApiFormat(values[AppFormField.periodeFra]!);
-    //     const toDateString = formatDateToApiFormat(values[AppFormField.periodeTil]!);
-    //     return getArbeidsgiver(fromDateString, toDateString);
-    // }
-
-    // handleArbeidsforholdFetchError(response: AxiosError) {
-    //     if (apiUtils.isForbidden(response) || apiUtils.isUnauthorized(response)) {
-    //         navigateToLoginPage();
-    //     }
-    // }
-
-    // async finishStep(søkerdata: Søkerdata) {
-    //     this.setState({ isLoadingNextStep: true });
-
-    //     if (appIsRunningInDemoMode()) {
-    //         søkerdata.setAnsettelsesforhold(demoSøkerdata.ansettelsesforhold);
-    //         navigateTo(this.props.nextStepRoute!, this.props.history);
-    //         return;
-    //     }
-    //     try {
-    //         const response = await this.getArbeidsforhold();
-    //         søkerdata.setAnsettelsesforhold!(response.data.organisasjoner);
-    //         if (this.props.formikProps.values[AppFormField.ansettelsesforhold].length === 0) {
-    //             this.props.formikProps.setFieldValue(AppFormField.ansettelsesforhold, [
-    //                 ...response.data.organisasjoner
-    //             ]);
-    //         }
-    //     } catch (error) {
-    //         this.handleArbeidsforholdFetchError(error);
-    //     }
-
-    //     const { nextStepRoute } = this.props;
-    //     if (nextStepRoute) {
-    //         navigateTo(nextStepRoute, this.props.history);
-    //     }
-    // }
-
-    // validateFraDato(fraDato?: Date) {
-    //     const { periodeTil } = this.props.formikProps.values;
-    //     return validateFradato(fraDato, periodeTil);
-    // }
-
-    // validateTilDato(tilDato?: Date) {
-    //     const { periodeFra } = this.props.formikProps.values;
-    //     return validateTildato(tilDato, periodeFra);
-    // }
-
-    // render() {
-    // const { history, intl, formikProps, ...stepProps } = this.props;
-    // const { isLoadingNextStep } = this.state;
     const navigate = nextStepRoute ? () => navigateTo(nextStepRoute, history) : undefined;
 
     const fraDato = formikProps.values[AppFormField.periodeFra];
