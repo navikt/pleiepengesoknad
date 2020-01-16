75526e1f0dedba0cb572739c348f209745aaebbc
Are Bjølseth <are.bjolseth@nav.com>
2020-01-16 14:10:37 +0100

    Lintet and changed tab == 2 spaces

--- src/app/components/steps/legeerklæring/LegeerklæringStep.tsx
+++ src/app/components/steps/legeerklæring/LegeerklæringStep.tsx
@@ -20,60 +20,60 @@ import CounsellorPanel from 'common/components/counsellor-panel/CounsellorPanel'
 import PictureScanningGuide from '../../../../common/components/picture-scanning-guide/PictureScanningGuide';
 
 type Props = { formikProps: CustomFormikProps } & CommonStepFormikProps &
-  HistoryProps &
-  InjectedIntlProps &
-  StepConfigProps;
+    HistoryProps &
+    InjectedIntlProps &
+    StepConfigProps;
 
 const LegeerklæringStep = ({history, intl, nextStepRoute, formikProps, ...stepProps}: Props) => {
-  const [filesThatDidntGetUploaded, setFilesThatDidntGetUploaded] = React.useState<File[]>([]);
+    const [filesThatDidntGetUploaded, setFilesThatDidntGetUploaded] = React.useState<File[]>([]);
 
-  const navigate = nextStepRoute ? () => navigateTo(nextStepRoute, history) : undefined;
-  const isRunningDemoMode = appIsRunningInDemoMode();
+    const navigate = nextStepRoute ? () => navigateTo(nextStepRoute, history) : undefined;
+    const isRunningDemoMode = appIsRunningInDemoMode();
 
-  return (
-    <FormikStep
-      id={StepID.LEGEERKLÆRING}
-      onValidFormSubmit={navigate}
-      history={history}
-      useValidationErrorSummary={false}
-      skipValidation={isRunningDemoMode}
-      {...stepProps}>
-      {isRunningDemoMode && (
-        <Box>
-          <AlertStripeInfo>
-            Opplasting av legeerklæring er ikke tilgjengelig i demo versjon. Du kan klikke Fortsett.
-          </AlertStripeInfo>
-        </Box>
-      )}
-      {false === isRunningDemoMode && (
-        <>
-          <Box padBottom="xl">
-            <CounsellorPanel>
-              <p><FormattedHTMLMessage id="steg.lege.intro.1.html"/></p>
-              <p><FormattedHTMLMessage id="steg.lege.intro.2.html"/></p>
-            </CounsellorPanel>
-          </Box>
-          <HelperTextPanel>
-            <PictureScanningGuide />
-          </HelperTextPanel>
-          <Box margin="l">
-            <FormikFileUploader
-              name={AppFormField.legeerklæring}
-              label={intlHelper(intl, 'steg.lege.vedlegg')}
-              onErrorUploadingAttachments={setFilesThatDidntGetUploaded}
-              onFileInputClick={() => {
-                setFilesThatDidntGetUploaded([]);
-              }}
-              validate={validateLegeerklæring}
-              onUnauthorizedOrForbiddenUpload={navigateToLoginPage}
-            />
-          </Box>
-          <FileUploadErrors filesThatDidntGetUploaded={filesThatDidntGetUploaded}/>
-          <LegeerklæringFileList wrapNoAttachmentsInBox={true} includeDeletionFunctionality={true}/>
-        </>
-      )}
-    </FormikStep>
-  );
+    return (
+        <FormikStep
+            id={StepID.LEGEERKLÆRING}
+            onValidFormSubmit={navigate}
+            history={history}
+            useValidationErrorSummary={false}
+            skipValidation={isRunningDemoMode}
+            {...stepProps}>
+            {isRunningDemoMode && (
+                <Box>
+                    <AlertStripeInfo>
+                        Opplasting av legeerklæring er ikke tilgjengelig i demo versjon. Du kan klikke Fortsett.
+                    </AlertStripeInfo>
+                </Box>
+            )}
+            {false === isRunningDemoMode && (
+                <>
+                    <Box padBottom="xl">
+                        <CounsellorPanel>
+                            <p><FormattedHTMLMessage id="steg.lege.intro.1.html"/></p>
+                            <p><FormattedHTMLMessage id="steg.lege.intro.2.html"/></p>
+                        </CounsellorPanel>
+                    </Box>
+                    <HelperTextPanel>
+                        <PictureScanningGuide/>
+                    </HelperTextPanel>
+                    <Box margin="l">
+                        <FormikFileUploader
+                            name={AppFormField.legeerklæring}
+                            label={intlHelper(intl, 'steg.lege.vedlegg')}
+                            onErrorUploadingAttachments={setFilesThatDidntGetUploaded}
+                            onFileInputClick={() => {
+                                setFilesThatDidntGetUploaded([]);
+                            }}
+                            validate={validateLegeerklæring}
+                            onUnauthorizedOrForbiddenUpload={navigateToLoginPage}
+                        />
+                    </Box>
+                    <FileUploadErrors filesThatDidntGetUploaded={filesThatDidntGetUploaded}/>
+                    <LegeerklæringFileList wrapNoAttachmentsInBox={true} includeDeletionFunctionality={true}/>
+                </>
+            )}
+        </FormikStep>
+    );
 };
 
 export default injectIntl(LegeerklæringStep);
--- src/common/components/picture-scanning-guide/PictureScanningGuide.tsx
+++ src/common/components/picture-scanning-guide/PictureScanningGuide.tsx
@@ -13,49 +13,51 @@ const bem = bemHelper('pictureScanningGuide');
 type Props = InjectedIntlProps;
 
 const PictureScanningGuide: React.FunctionComponent<Props> = ({intl}) => {
-  const svgIconSize = 100;
-  return (
-    <>
-      <FormattedHTMLMessage tagName="h2" id="psg.innholdstittel"/>
-      <FormattedHTMLMessage tagName="h3" id="psg.section1.tittel"/>
-      <FormattedHTMLMessage tagName="ul" id="psg.section1.liste"/>
-      <FormattedHTMLMessage tagName="h3" id="psg.section2.tittel"/>
-      <FormattedHTMLMessage tagName="ul" id="psg.section2.liste"/>
-      <div className={bem.block}>
-        <h3><FormattedMessage id="psg.icon.heading" /></h3>
-        <div className={bem.element('body')}>
-          <div className={bem.element('cell')}>
-            <ScanningIcon status="good" size={svgIconSize} />
-            <div className={bem.element('text-block')}>
-              <p><StatusIkon status="suksess"/><FormattedHTMLMessage tagName="strong" id="psg.good" /></p>
-              <FormattedHTMLMessage tagName="span" id="psg.icon.label.good" />
+    const svgIconSize = 100;
+    return (
+        <>
+            <FormattedHTMLMessage tagName="h2" id="psg.innholdstittel"/>
+            <FormattedHTMLMessage tagName="h3" id="psg.section1.tittel"/>
+            <FormattedHTMLMessage tagName="ul" id="psg.section1.liste"/>
+            <FormattedHTMLMessage tagName="h3" id="psg.section2.tittel"/>
+            <FormattedHTMLMessage tagName="ul" id="psg.section2.liste"/>
+            <div className={bem.block}>
+                <h3><FormattedMessage id="psg.icon.heading"/></h3>
+                <div className={bem.element('body')}>
+                    <div className={bem.element('cell')}>
+                        <ScanningIcon status="good" size={svgIconSize}/>
+                        <div className={bem.element('text-block')}>
+                            <p><StatusIkon status="suksess"/><FormattedHTMLMessage tagName="strong" id="psg.good"/></p>
+                            <FormattedHTMLMessage tagName="span" id="psg.icon.label.good"/>
+                        </div>
+                    </div>
+                    <div className={bem.element('cell')}>
+                        <ScanningIcon status="keystone" size={svgIconSize}/>
+                        <div className={bem.element('text-block')}>
+                            <p><StatusIkon status="feil"/><FormattedHTMLMessage tagName="strong" id="psg.bad"/></p>
+                            <FormattedHTMLMessage tagName="span" id="psg.icon.label.keystone"/>
+                        </div>
+                    </div>
+                    <div className={bem.element('cell')}>
+                        <ScanningIcon status="horizontal" size={svgIconSize}/>
+                        <div className={bem.element('text-block')}>
+                            <p><StatusIkon status="feil"/><FormattedHTMLMessage tagName="strong" id="psg.bad"/></p>
+                            <FormattedHTMLMessage tagName="span" id="psg.icon.label.horizontal"/>
+                        </div>
+                    </div>
+                    <div className={bem.element('cell')}>
+                        <ScanningIcon status="shadow" size={svgIconSize}/>
+                        <div className={bem.element('text-block')}>
+                            <p><StatusIkon status="feil"/><FormattedHTMLMessage tagName="strong" id="psg.bad"/></p>
+                            <FormattedHTMLMessage tagName="span" id="psg.icon.label.shadow"/>
+                        </div>
+                    </div>
+                </div>
+                <Lenkepanel tittelProps="normaltekst" target="_blank" href={intlHelper(intl, 'psg.lenkepanel.url')}>
+                    <FormattedMessage id="psg.lenkepanel.text"/>
+                </Lenkepanel>
             </div>
-          </div>
-          <div className={bem.element('cell')}>
-            <ScanningIcon status="keystone" size={svgIconSize} />
-            <div className={bem.element('text-block')}>
-              <p><StatusIkon status="feil"/><FormattedHTMLMessage tagName="strong" id="psg.bad" /></p>
-              <FormattedHTMLMessage tagName="span" id="psg.icon.label.keystone" />
-            </div>
-          </div>
-          <div className={bem.element('cell')}>
-            <ScanningIcon status="horizontal" size={svgIconSize} />
-            <div className={bem.element('text-block')}>
-              <p><StatusIkon status="feil"/><FormattedHTMLMessage tagName="strong" id="psg.bad" /></p>
-              <FormattedHTMLMessage tagName="span" id="psg.icon.label.horizontal" />
-            </div>
-          </div>
-          <div className={bem.element('cell')}>
-            <ScanningIcon status="shadow" size={svgIconSize} />
-            <div className={bem.element('text-block')}>
-              <p><StatusIkon status="feil"/><FormattedHTMLMessage tagName="strong" id="psg.bad" /></p>
-              <FormattedHTMLMessage tagName="span" id="psg.icon.label.shadow" />
-            </div>
-          </div>
-        </div>
-        <Lenkepanel tittelProps="normaltekst" target="_blank" href={intlHelper(intl, 'psg.lenkepanel.url')}><FormattedMessage id="psg.lenkepanel.text" /></Lenkepanel>
-      </div>
-      </>
-  );
+        </>
+    );
 };
 export default injectIntl(PictureScanningGuide);
