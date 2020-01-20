a5a8917f8bf0a11912a4db36b74af0a3fe75c239
Are Bjølseth <are.bjolseth@nav.com>
2020-01-20 10:29:05 +0100

    Linting with tabs == 4 spaces.

--- src/app/components/steps/legeerklæring/LegeerklæringStep.tsx
+++ src/app/components/steps/legeerklæring/LegeerklæringStep.tsx
@@ -1,20 +1,20 @@
 import * as React from 'react';
-import {injectIntl, InjectedIntlProps, FormattedHTMLMessage} from 'react-intl';
-import {StepID, StepConfigProps} from '../../../config/stepConfig';
-import {HistoryProps} from 'common/types/History';
-import {navigateTo, navigateToLoginPage} from '../../../utils/navigationUtils';
+import { injectIntl, InjectedIntlProps, FormattedHTMLMessage } from 'react-intl';
+import { StepID, StepConfigProps } from '../../../config/stepConfig';
+import { HistoryProps } from 'common/types/History';
+import { navigateTo, navigateToLoginPage } from '../../../utils/navigationUtils';
 import FormikStep from '../../formik-step/FormikStep';
 import LegeerklæringFileList from '../../legeerklæring-file-list/LegeerklæringFileList';
 import FormikFileUploader from '../../formik-file-uploader/FormikFileUploader';
-import {AppFormField} from '../../../types/PleiepengesøknadFormData';
+import { AppFormField } from '../../../types/PleiepengesøknadFormData';
 import FileUploadErrors from '../../file-upload-errors/FileUploadErrors';
-import {validateLegeerklæring} from '../../../validation/fieldValidations';
+import { validateLegeerklæring } from '../../../validation/fieldValidations';
 import intlHelper from 'common/utils/intlUtils';
 import Box from 'common/components/box/Box';
-import {CommonStepFormikProps} from '../../pleiepengesøknad-content/PleiepengesøknadContent';
-import {AlertStripeInfo} from 'nav-frontend-alertstriper';
-import {appIsRunningInDemoMode} from '../../../utils/envUtils';
-import {CustomFormikProps} from '../../../types/FormikProps';
+import { CommonStepFormikProps } from '../../pleiepengesøknad-content/PleiepengesøknadContent';
+import { AlertStripeInfo } from 'nav-frontend-alertstriper';
+import { appIsRunningInDemoMode } from '../../../utils/envUtils';
+import { CustomFormikProps } from '../../../types/FormikProps';
 import HelperTextPanel from 'common/components/helper-text-panel/HelperTextPanel';
 import CounsellorPanel from 'common/components/counsellor-panel/CounsellorPanel';
 import PictureScanningGuide from '../../../../common/components/picture-scanning-guide/PictureScanningGuide';
@@ -24,7 +24,7 @@ type Props = { formikProps: CustomFormikProps } & CommonStepFormikProps &
     InjectedIntlProps &
     StepConfigProps;
 
-const LegeerklæringStep = ({history, intl, nextStepRoute, formikProps, ...stepProps}: Props) => {
+const LegeerklæringStep = ({ history, intl, nextStepRoute, formikProps, ...stepProps }: Props) => {
     const [filesThatDidntGetUploaded, setFilesThatDidntGetUploaded] = React.useState<File[]>([]);
 
     const navigate = nextStepRoute ? () => navigateTo(nextStepRoute, history) : undefined;
@@ -49,8 +49,12 @@ const LegeerklæringStep = ({history, intl, nextStepRoute, formikProps, ...stepP
                 <>
                     <Box padBottom="xl">
                         <CounsellorPanel>
-                            <p><FormattedHTMLMessage id="steg.lege.intro.1.html"/></p>
-                            <p><FormattedHTMLMessage id="steg.lege.intro.2.html"/></p>
+                            <p>
+                                <FormattedHTMLMessage id="steg.lege.intro.1.html" />
+                            </p>
+                            <p>
+                                <FormattedHTMLMessage id="steg.lege.intro.2.html" />
+                            </p>
                         </CounsellorPanel>
                     </Box>
                     <HelperTextPanel>
@@ -68,8 +72,8 @@ const LegeerklæringStep = ({history, intl, nextStepRoute, formikProps, ...stepP
                             onUnauthorizedOrForbiddenUpload={navigateToLoginPage}
                         />
                     </Box>
-                    <FileUploadErrors filesThatDidntGetUploaded={filesThatDidntGetUploaded}/>
-                    <LegeerklæringFileList wrapNoAttachmentsInBox={true} includeDeletionFunctionality={true}/>
+                    <FileUploadErrors filesThatDidntGetUploaded={filesThatDidntGetUploaded} />
+                    <LegeerklæringFileList wrapNoAttachmentsInBox={true} includeDeletionFunctionality={true} />
                 </>
             )}
         </FormikStep>
