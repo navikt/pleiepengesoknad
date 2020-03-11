74d82041944e58e4ed1aac01d16218bfa4347a62
Are Bjølseth <are.bjolseth@nav.com>
2020-03-11 14:30:50 +0100

    Fikset bug i start-dev.js, fjernet PORT=8082 fra server/package.json => start-api-mock La til CYPRESS_INSTALL_BINARY=0 i Dockerfile.web Fjeret env settings i script targets, pga defaults.

--- src/build/scripts/start-dev.js
+++ src/build/scripts/start-dev.js
@@ -1,3 +1,4 @@
+const process = require('process');
 const webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
 const webpackConfig = require('../webpack/webpack.config.dev');
@@ -14,7 +15,7 @@ getDecorator().then((decoratorData) => {
     const compiler = webpack(webpackConfig);
     const server = new WebpackDevServer(compiler, configureDevServer(decoratorData));
 
-    const host = `${process.env.HOST}` || '127.0.0.1';
+    const host = process.env.HOST || '127.0.0.1';
     const port = process.env.PORT || 8080;
     server.listen(port, host, () => console.log(`Started WebpackDevServer on http://${host}:${port}`));
 });
