664edb5e9bea340caf20a9fdcc7912fb9c54112d
Frode Hansen <frode.hansen@bekk.no>
2020-05-12 14:13:31 +0200

    Update link

--- src/app/lenker.ts
+++ src/app/lenker.ts
@@ -11,7 +11,8 @@ interface Lenker {
 const LenkerBokmål: Lenker = {
     medlemskap:
         'https://www.nav.no/no/Person/Flere+tema/Arbeid+og+opphold+i+Norge/Relatert+informasjon/medlemskap-i-folketrygden',
-    papirskjemaPrivat: 'https://www.nav.no/soknader/nb/person/familie/omsorgspenger/NAV%2009-11.05/brev',
+    papirskjemaPrivat:
+        'https://www.nav.no/soknader/nb/person/familie/pleiepenger-og-opplaringspenger/NAV%2009-11.05/brev',
     vilkårPleiepenger: 'https://www.nav.no/no/Person/Familie/Sykdom+i+familien/pleiepenger+for+pleie+av+sykt+barn',
     personvern:
         'https://www.nav.no/no/NAV+og+samfunn/Om+NAV/personvern-i-arbeids-og-velferdsetaten/personvernerkl%C3%A6ring-for-arbeids-og-velferdsetaten',
@@ -22,7 +23,8 @@ const LenkerBokmål: Lenker = {
 
 const LenkerNynorsk: Partial<Lenker> = {
     medlemskap: 'https://www.nav.no/no/Person/Flere+tema/Arbeid+og+opphold+i+Norge/Nynorsk/medlemskap-i-folketrygda',
-    papirskjemaPrivat: 'https://www.nav.no/soknader/nb/person/familie/omsorgspenger/NAV%2009-11.05/brev',
+    papirskjemaPrivat:
+        'https://www.nav.no/soknader/nb/person/familie/pleiepenger-og-opplaringspenger/NAV%2009-11.05/brev',
     vilkårPleiepenger:
         'https://www.nav.no/no/Person/Familie/Sykdom+i+familien/Nynorsk/pleiepengar+for+pleie+av+sjukt+barn',
     rettOgPlikt:
