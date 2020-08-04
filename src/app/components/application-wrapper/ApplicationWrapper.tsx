807be8453308ce712eec1ed940ba93be28689cb7
Frode Hansen <frode.hansen@bekk.no>
2020-08-04 18:14:08 +0200

    Add route for showing texts

--- src/app/components/application-wrapper/ApplicationWrapper.tsx
+++ src/app/components/application-wrapper/ApplicationWrapper.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Normaltekst } from 'nav-frontend-typografi';
 import LanguageToggle from 'common/components/language-toggle/LanguageToggle';
+import FullscreenContainer from 'common/dev-utils/fullscreenContainer/FullscreenContainer';
 import { Locale } from 'common/types/Locale';
-import IntlProvider from '../intl-provider/IntlProvider';
+import IntlProvider, { appBokmålstekster, appNynorsktekster } from '../intl-provider/IntlProvider';
+import MessagesPreview from 'common/dev-utils/intl/messages-preview/MessagesPreview';
 
 interface ApplicationWrapperProps {
     locale: Locale;
@@ -15,7 +17,22 @@ const ApplicationWrapper = ({ locale, onChangeLocale, children }: ApplicationWra
         <IntlProvider locale={locale}>
             <Normaltekst tag="div">
                 <LanguageToggle locale={locale} toggle={onChangeLocale} />
-                <Router>{children}</Router>
+                <Router>
+                    {children}
+                    <Route path="*/dev/tekster">
+                        <FullscreenContainer>
+                            <MessagesPreview
+                                title="Søknad pleiepenger"
+                                showMissingTextSummary={false}
+                                showExplanation={false}
+                                messages={{
+                                    nb: appBokmålstekster,
+                                    nn: appNynorsktekster,
+                                }}
+                            />
+                        </FullscreenContainer>
+                    </Route>
+                </Router>
             </Normaltekst>
         </IntlProvider>
     );
--- src/app/components/intl-provider/IntlProvider.tsx
+++ src/app/components/intl-provider/IntlProvider.tsx
@@ -5,29 +5,26 @@ import '@formatjs/intl-pluralrules/dist/locale-data/nn';
 import '@formatjs/intl-pluralrules/polyfill';
 import bostedMessages from '@navikt/sif-common-forms/lib/bosted-utland/bostedUtlandMessages';
 import utenlandsoppholdMessages from '@navikt/sif-common-forms/lib/utenlandsopphold/utenlandsoppholdMessages';
-import MessagesPreview from 'common/dev-utils/intl/messages-preview/MessagesPreview';
+import virksomhetMessages from '@navikt/sif-common-forms/lib/virksomhet/virksomhetMessages';
 import { allCommonMessages } from 'common/i18n/allCommonMessages';
 import { Locale } from 'common/types/Locale';
 
-const selvstendigMessagesNb = require('../../i18n/selvstendigOgFrilans.nb.json');
-const selvstendigMessagesNn = require('../../i18n/selvstendigOgFrilans.nn.json');
-
-const appBokmålstekster = require('../../i18n/nb.json');
-const appNynorsktekster = require('../../i18n/nn.json');
+export const appBokmålstekster = require('../../i18n/nb.json');
+export const appNynorsktekster = require('../../i18n/nn.json');
 
 const bokmålstekster = {
     ...allCommonMessages.nb,
     ...utenlandsoppholdMessages.nb,
-    ...appBokmålstekster,
     ...bostedMessages.nb,
-    ...selvstendigMessagesNb,
+    ...virksomhetMessages.nb,
+    ...appBokmålstekster,
 };
 const nynorsktekster = {
     ...allCommonMessages.nn,
     ...utenlandsoppholdMessages.nn,
-    ...appNynorsktekster,
     ...bostedMessages.nn,
-    ...selvstendigMessagesNn,
+    ...virksomhetMessages.nn,
+    ...appNynorsktekster,
 };
 
 export interface IntlProviderProps {
@@ -36,23 +33,11 @@ export interface IntlProviderProps {
     onError?: (error: any) => void;
 }
 
-const showMessages = false;
-
 const IntlProvider = ({ locale, onError, children }: IntlProviderProps) => {
     const messages = locale === 'nb' ? bokmålstekster : nynorsktekster;
     return (
         <Provider locale={locale} messages={messages} onError={onError}>
             {children}
-            {showMessages && (
-                <MessagesPreview
-                    title="Søknad pleiepenger"
-                    showMissingTextSummary={false}
-                    messages={{
-                        nb: bokmålstekster,
-                        nn: nynorsktekster,
-                    }}
-                />
-            )}
         </Provider>
     );
 };
