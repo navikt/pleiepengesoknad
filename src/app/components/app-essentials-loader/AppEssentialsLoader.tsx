142cfd31a49f9bceac092352a972dd131daddc11
Andre Wolden <wolden.andre@gmail.com>
2020-08-12 21:19:09 +0200

    endrer AppEssentialsLoader til function compoenent

--- src/app/components/app-essentials-loader/AppEssentialsLoader.tsx
+++ src/app/components/app-essentials-loader/AppEssentialsLoader.tsx
@@ -12,6 +12,7 @@ import * as apiUtils from '../../utils/apiUtils';
 import appSentryLogger from '../../utils/appSentryLogger';
 import { navigateToLoginPage, userIsCurrentlyOnErrorPage } from '../../utils/navigationUtils';
 import LoadingPage from '../pages/loading-page/LoadingPage';
+import { useEffect, useState } from 'react';
 
 export const VERIFY_MELLOMLAGRING_VERSION = true;
 
@@ -24,11 +25,19 @@ interface Props {
 }
 
 interface State {
+    initialRender: boolean;
     isLoading: boolean;
     lastStepID?: StepID;
     formdata: PleiepengesøknadFormData;
     søkerdata?: Søkerdata;
 }
+
+const initialState: State = {
+    initialRender: true,
+    isLoading: true,
+    lastStepID: undefined,
+    formdata: initialValues,
+};
 
 const getValidAttachments = (attachments: Attachment[] = []): Attachment[] => {
     return attachments.filter((a) => {
@@ -36,37 +45,20 @@ const getValidAttachments = (attachments: Attachment[] = []): Attachment[] => {
     });
 };
 
-class AppEssentialsLoader extends React.Component<Props, State> {
-    constructor(props: Props) {
-        super(props);
-        this.state = {
-            isLoading: true,
-            lastStepID: undefined,
-            formdata: initialValues,
-        };
+const AppEssentialsLoader = (props: Props): React.ReactElement => {
+    const { contentLoadedRenderer } = props;
 
-        this.updateArbeidsgivere = this.updateArbeidsgivere.bind(this);
-        this.updateSøkerdata = this.updateSøkerdata.bind(this);
-        this.stopLoading = this.stopLoading.bind(this);
-        this.handleSøkerdataFetchSuccess = this.handleSøkerdataFetchSuccess.bind(this);
-        this.handleSøkerdataFetchError = this.handleSøkerdataFetchError.bind(this);
-        this.loadAppEssentials();
+    const [state, setState] = useState(initialState);
+    const { initialRender, isLoading, lastStepID, formdata, søkerdata } = state;
+
+    function stopLoading() {
+        setState({
+            ...state,
+            isLoading: false,
+        });
     }
 
-    async loadAppEssentials() {
-        try {
-            const [mellomlagringResponse, søkerResponse, barnResponse] = await Promise.all([
-                rehydrate(),
-                getSøker(),
-                getBarn(),
-            ]);
-            this.handleSøkerdataFetchSuccess(mellomlagringResponse, søkerResponse, barnResponse);
-        } catch (error) {
-            this.handleSøkerdataFetchError(error);
-        }
-    }
-
-    getValidMellomlagring = (data?: MellomlagringData): MellomlagringData | undefined => {
+    function getValidMellomlagring(data?: MellomlagringData): MellomlagringData | undefined {
         if (VERIFY_MELLOMLAGRING_VERSION) {
             if (data?.metadata?.version === MELLOMLAGRING_VERSION) {
                 return data;
@@ -74,79 +66,27 @@ class AppEssentialsLoader extends React.Component<Props, State> {
             return undefined;
         }
         return data;
-    };
-
-    handleSøkerdataFetchSuccess(
-        mellomlagringResponse: AxiosResponse,
-        søkerResponse: AxiosResponse,
-        barnResponse?: AxiosResponse
-    ) {
-        const mellomlagring = this.getValidMellomlagring(mellomlagringResponse?.data);
-        const formData = mellomlagring?.formData
-            ? {
-                  ...mellomlagring.formData,
-                  [AppFormField.legeerklæring]: getValidAttachments(mellomlagring.formData.legeerklæring),
-              }
-            : undefined;
-        const lastStepID = mellomlagring?.metadata?.lastStepID;
-        this.updateSøkerdata(
-            formData || { ...initialValues },
-            {
-                person: søkerResponse.data,
-                barn: barnResponse ? barnResponse.data.barn : undefined,
-                setArbeidsgivere: this.updateArbeidsgivere,
-                arbeidsgivere: [],
-            },
-            lastStepID,
-            () => {
-                this.stopLoading();
-                if (userIsCurrentlyOnErrorPage()) {
-                    window.location.assign(routeConfig.WELCOMING_PAGE_ROUTE);
-                }
-            }
-        );
     }
 
-    updateSøkerdata(
-        formdata: PleiepengesøknadFormData,
-        søkerdata: Søkerdata,
-        lastStepID?: StepID,
-        callback?: () => void
-    ) {
-        this.setState(
-            {
-                isLoading: false,
-                lastStepID: lastStepID || this.state.lastStepID,
-                formdata: formdata || this.state.formdata,
-                søkerdata: søkerdata || this.state.søkerdata,
-            },
-            callback
-        );
-    }
-
-    stopLoading() {
-        this.setState({
+    function updateSøkerdata(formdata: PleiepengesøknadFormData, søkerdata: Søkerdata, lastStepID?: StepID) {
+        setState({
+            ...state,
             isLoading: false,
+            lastStepID: lastStepID || state.lastStepID,
+            formdata: formdata || state.formdata,
+            søkerdata: søkerdata || state.søkerdata,
         });
-    }
-
-    handleSøkerdataFetchError(error: AxiosError) {
-        if (apiUtils.isForbidden(error) || apiUtils.isUnauthorized(error)) {
-            navigateToLoginPage();
-        } else if (!userIsCurrentlyOnErrorPage()) {
-            appSentryLogger.logApiError(error);
-            window.location.assign(routeConfig.ERROR_PAGE_ROUTE);
+        stopLoading();
+        if (userIsCurrentlyOnErrorPage()) {
+            window.location.assign(routeConfig.WELCOMING_PAGE_ROUTE);
         }
-        // this timeout is set because if isLoading is updated in the state too soon,
-        // the contentLoadedRenderer() will be called while the user is still on the wrong route,
-        // because the redirect to routeConfig.ERROR_PAGE_ROUTE will not have happened yet.
-        setTimeout(this.stopLoading, 200);
     }
 
-    updateArbeidsgivere(arbeidsgivere: Arbeidsgiver[]) {
-        if (this.state.søkerdata) {
-            const { barn, person, setArbeidsgivere } = this.state.søkerdata;
-            this.setState({
+    function updateArbeidsgivere(arbeidsgivere: Arbeidsgiver[]) {
+        if (state.søkerdata) {
+            const { barn, person, setArbeidsgivere } = state.søkerdata;
+            setState({
+                ...state,
                 søkerdata: {
                     barn,
                     setArbeidsgivere,
@@ -157,20 +97,74 @@ class AppEssentialsLoader extends React.Component<Props, State> {
         }
     }
 
-    render() {
-        const { contentLoadedRenderer } = this.props;
-        const { isLoading, lastStepID, formdata, søkerdata } = this.state;
-        if (isLoading) {
-            return <LoadingPage />;
-        }
-        return (
-            <>
-                <SøkerdataContextProvider value={søkerdata}>
-                    {contentLoadedRenderer(formdata, lastStepID, søkerdata)}
-                </SøkerdataContextProvider>
-            </>
+    function handleSøkerdataFetchSuccess(
+        mellomlagringResponse: AxiosResponse,
+        søkerResponse: AxiosResponse,
+        barnResponse?: AxiosResponse
+    ) {
+        const mellomlagring = getValidMellomlagring(mellomlagringResponse?.data);
+        const formData = mellomlagring?.formData
+            ? {
+                  ...mellomlagring.formData,
+                  [AppFormField.legeerklæring]: getValidAttachments(mellomlagring.formData.legeerklæring),
+              }
+            : undefined;
+        const lastStepID = mellomlagring?.metadata?.lastStepID;
+        updateSøkerdata(
+            formData || { ...initialValues },
+            {
+                person: søkerResponse.data,
+                barn: barnResponse ? barnResponse.data.barn : undefined,
+                setArbeidsgivere: updateArbeidsgivere,
+                arbeidsgivere: [],
+            },
+            lastStepID
         );
     }
-}
+
+    function handleSøkerdataFetchError(error: AxiosError) {
+        if (apiUtils.isForbidden(error) || apiUtils.isUnauthorized(error)) {
+            navigateToLoginPage();
+        } else if (!userIsCurrentlyOnErrorPage()) {
+            appSentryLogger.logApiError(error);
+            window.location.assign(routeConfig.ERROR_PAGE_ROUTE);
+        }
+        // this timeout is set because if isLoading is updated in the state too soon,
+        // the contentLoadedRenderer() will be called while the user is still on the wrong route,
+        // because the redirect to routeConfig.ERROR_PAGE_ROUTE will not have happened yet.
+        setTimeout(stopLoading, 200);
+    }
+
+    async function loadAppEssentials() {
+        try {
+            const [mellomlagringResponse, søkerResponse, barnResponse] = await Promise.all([
+                rehydrate(),
+                getSøker(),
+                getBarn(),
+            ]);
+            handleSøkerdataFetchSuccess(mellomlagringResponse, søkerResponse, barnResponse);
+        } catch (error) {
+            handleSøkerdataFetchError(error);
+        }
+    }
+
+    useEffect(() => {
+        if (initialRender) {
+            setState({ ...state, initialRender: false });
+            loadAppEssentials();
+        }
+    });
+
+    if (isLoading) {
+        return <LoadingPage />;
+    }
+    return (
+        <>
+            <SøkerdataContextProvider value={søkerdata}>
+                {contentLoadedRenderer(formdata, lastStepID, søkerdata)}
+            </SøkerdataContextProvider>
+        </>
+    );
+};
 
 export default AppEssentialsLoader;
