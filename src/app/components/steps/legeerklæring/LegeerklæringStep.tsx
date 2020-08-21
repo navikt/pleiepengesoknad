7dbf733f628df24d0c2e2bebd6fc22aaafa3a1cb
Andre Wolden <wolden.andre@gmail.com>
2020-08-21 11:49:42 +0200

    counsellorpanel tekst endring. ny PSG

--- src/app/components/steps/legeerklæring/LegeerklæringStep.tsx
+++ src/app/components/steps/legeerklæring/LegeerklæringStep.tsx
@@ -1,11 +1,10 @@
 import * as React from 'react';
-import { useIntl } from 'react-intl';
+import { FormattedMessage, useIntl } from 'react-intl';
 import { useFormikContext } from 'formik';
 import Lenke from 'nav-frontend-lenker';
 import Box from '@sif-common/core/components/box/Box';
 import CounsellorPanel from '@sif-common/core/components/counsellor-panel/CounsellorPanel';
 import FileUploadErrors from '@sif-common/core/components/file-upload-errors/FileUploadErrors';
-import HelperTextPanel from '@sif-common/core/components/helper-text-panel/HelperTextPanel';
 import PictureScanningGuide from '@sif-common/core/components/picture-scanning-guide/PictureScanningGuide';
 import { Attachment } from '@sif-common/core/types/Attachment';
 import { mapFileToPersistedFile } from '@sif-common/core/utils/attachmentUtils';
@@ -61,35 +60,45 @@ const LegeerklæringStep = ({ onValidSubmit }: StepConfigProps) => {
             buttonDisabled={hasPendingUploads}>
             <Box padBottom="xl">
                 <CounsellorPanel>
-                    <p>
-                        Regelverket for pleiepenger er <strong>ikke</strong> endret i forhold til koronasituasjonen.
-                    </p>
-                    <p>For å få pleiepenger må barnet</p>
-                    <ul>
-                        <li style={{ marginBottom: '.5rem' }}>
-                            ha vært til behandling eller utredning i sykehus eller annen spesialisthelsetjeneste
-                        </li>
-                        <li>ha pleie hele tiden på grunn av sykdom</li>
-                    </ul>
-                    <p>
-                        Her skal du laste opp legeerklæring <strong>eller</strong> en bekreftelse på at barnet har vært
-                        til behandling/utredning i sykehus eller annen spesialisthelsetjeneste.
-                    </p>
-                    <p>
-                        Vi har forståelse for at det kan være vanskelig å skaffe legeerklæring/bekreftelse på grunn av
-                        koronasituasjonen. Hvis du ikke har dokumentasjonen nå, kan du ettersende den.{' '}
+                    <Box padBottom={'l'}>
+                        <FormattedMessage id={'steg.legeerklaering.counsellorpanel.1.1'} />
+                        <FormattedMessage id={'steg.legeerklaering.counsellorpanel.1.2'} />
+                        <FormattedMessage id={'steg.legeerklaering.counsellorpanel.1.3'} />
+                    </Box>
+
+                    <Box padBottom={'l'}>
+                        <FormattedMessage id={'steg.legeerklaering.counsellorpanel.2'} />
+                        <ul>
+                            <li>
+                                <FormattedMessage id={'steg.legeerklaering.counsellorpanel.2.1'} />
+                            </li>
+                            <li>
+                                <FormattedMessage id={'steg.legeerklaering.counsellorpanel.2.2'} />
+                            </li>
+                        </ul>
+                    </Box>
+
+                    <Box padBottom={'l'}>
+                        <FormattedMessage id={'steg.legeerklaering.counsellorpanel.3.1'} />
+                        <FormattedMessage id={'steg.legeerklaering.counsellorpanel.3.2'} />
+                        <FormattedMessage id={'steg.legeerklaering.counsellorpanel.3.3'} />
+                    </Box>
+
+                    <Box padBottom={'l'}>
+                        <FormattedMessage id={'steg.legeerklaering.counsellorpanel.4'} />
                         <Lenke
                             href="https://www.nav.no/soknader/nb/person/familie/pleiepenger-og-opplaringspenger/NAV%2009-11.05/ettersendelse"
-                            target="_blank">
-                            Her får du veiledning til hvordan du ettersender dokumentasjon
+                            target="_blank"
+                            rel={'noopener'}>
+                            <FormattedMessage id={'steg.legeerklaering.counsellorpanel.4.1'} />
                         </Lenke>
-                    </p>
+                    </Box>
                 </CounsellorPanel>
             </Box>
 
-            <HelperTextPanel>
+            <Box margin={'l'}>
                 <PictureScanningGuide />
-            </HelperTextPanel>
+            </Box>
 
             <Box margin="l">
                 <FormikFileUploader
