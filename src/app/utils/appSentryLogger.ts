1a0f306dbac3a1f1fc30aab72cc0423edb62a4b4
Frode Hansen <frode.hansen@bekk.no>
2020-09-28 09:24:08 +0200

    Add allowUrl filter for sentry logging

--- src/app/utils/appSentryLogger.ts
+++ src/app/utils/appSentryLogger.ts
@@ -1,5 +1,5 @@
 import getSentryLoggerForApp from '@navikt/sif-common-sentry';
 
-const appSentryLogger = getSentryLoggerForApp('pleiepengesoknad');
+const appSentryLogger = getSentryLoggerForApp('pleiepengesoknad', ['sykdom-i-familien']);
 
 export default appSentryLogger;
