350e2a7bc278febe3a1cc17165c75201c85d38f7
Frode Hansen <frode.hansen@bekk.no>
2020-09-28 11:26:13 +0200

    Update path

--- src/app/utils/appSentryLogger.ts
+++ src/app/utils/appSentryLogger.ts
@@ -1,5 +1,5 @@
 import getSentryLoggerForApp from '@navikt/sif-common-sentry';
 
-const appSentryLogger = getSentryLoggerForApp('pleiepengesoknad', ['pleiepengesoknad.nav.no']);
+const appSentryLogger = getSentryLoggerForApp('pleiepengesoknad', ['pleiepengesoknad.nav.no/dist/js']);
 
 export default appSentryLogger;
