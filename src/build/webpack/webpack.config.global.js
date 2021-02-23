4be923b613d3fc9d9d6be910b12c2e02c9d0e05b
Aleksei Poroshin <aleksei.poroshin@nav.no>
2021-02-23 14:09:45 +0100

    Endret publicPath i webpack

--- src/build/webpack/webpack.config.global.js
+++ src/build/webpack/webpack.config.global.js
@@ -10,7 +10,7 @@ const webpackConfig = {
     output: {
         path: path.resolve(__dirname, './../../../dist'),
         filename: 'js/[name].js',
-        publicPath: '../dist/',
+        publicPath: 'dist',
     },
     resolve: {
         extensions: ['.ts', '.tsx', '.js', '.json', '.jsx'],
