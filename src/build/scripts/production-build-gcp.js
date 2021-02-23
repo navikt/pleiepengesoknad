e1da98d71eb06fcac5171c77fd5ca76bbf69b5a1
Frode Hansen <frode.hansen@bekk.no>
2021-02-23 20:23:46 +0100

    Hm

--- src/build/scripts/production-build-gcp.js
+++ src/build/scripts/production-build-gcp.js
@@ -1,6 +1,6 @@
 const webpack = require('webpack');
 const webpackConfig = require('../webpack/webpack.config.production');
-webpackConfig.output.publicPath = '/familie/sykdom-i-familien/soknad/pleiepenger';
+webpackConfig.output.publicPath = '/familie/sykdom-i-familien/soknad/pleiepenger/dist';
 
 return webpack(webpackConfig, (err, stats) => {
     if (err || (stats.compilation.errors && stats.compilation.errors.length > 0)) {
