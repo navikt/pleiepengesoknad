cefe709352cb30afeb7a2f86bd55d39bff7a40e0
Frode Hansen <frode.hansen@bekk.no>
2021-03-03 09:47:30 +0100

    Update intropage

--- src/app/components/pages/intro-page/IntroForm.tsx
+++ src/app/components/pages/intro-page/IntroForm.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { useIntl } from 'react-intl';
+import { FormattedMessage, useIntl } from 'react-intl';
 import { YesOrNo } from '@navikt/sif-common-core/lib/types/YesOrNo';
 import { commonFieldErrorRenderer } from '@navikt/sif-common-core/lib/utils/commonFieldErrorRenderer';
 import intlHelper from '@navikt/sif-common-core/lib/utils/intlUtils';
 import { validateYesOrNoIsAnswered } from '@navikt/sif-common-core/lib/validation/fieldValidations';
-import { getTypedFormComponents } from '@navikt/sif-common-formik';
+import { getTypedFormComponents, UnansweredQuestionsInfo } from '@navikt/sif-common-formik';
 import FormQuestion from '@navikt/sif-common-soknad/lib/form-question/FormQuestion';
 import { IntroFormData, IntroFormField, introFormInitialValues } from './introFormConfig';
+import { Element } from 'nav-frontend-typografi';
+import { isYesOrNoAnswered } from '../../../validation/fieldValidations';
 
 interface Props {
     onValidSubmit: () => void;
@@ -20,17 +22,27 @@ const IntroForm: React.FunctionComponent<Props> = ({ onValidSubmit }) => {
         <IntroFormComponents.FormikWrapper
             initialValues={introFormInitialValues}
             onSubmit={() => {
-                console.log('whoa');
-
                 onValidSubmit();
             }}
             renderForm={({ values: { harLegeerklæring, erArbeidstaker } }) => {
+                const legeerklæringBesvart = isYesOrNoAnswered(harLegeerklæring);
+                const arbeidstakerBesvart = isYesOrNoAnswered(erArbeidstaker);
+                const kanFortsette: boolean = legeerklæringBesvart && arbeidstakerBesvart;
                 return (
                     <section aria-label="Se om du kan bruke det dette skjemaet:">
                         <IntroFormComponents.Form
                             runDelayedFormValidation={true}
                             includeValidationSummary={true}
-                            includeButtons={true}
+                            includeButtons={kanFortsette}
+                            noButtonsContentRenderer={
+                                kanFortsette
+                                    ? undefined
+                                    : () => (
+                                          <UnansweredQuestionsInfo>
+                                              <FormattedMessage id="ubesvarteSpørsmålInfo" />
+                                          </UnansweredQuestionsInfo>
+                                      )
+                            }
                             fieldErrorRenderer={(error) => commonFieldErrorRenderer(intl, error)}
                             submitButtonLabel={intlHelper(intl, 'introForm.start')}>
                             <FormQuestion
@@ -44,13 +56,30 @@ const IntroForm: React.FunctionComponent<Props> = ({ onValidSubmit }) => {
                                         : intlHelper(intl, 'introForm.form.harLegeerklæring.nei.info')
                                 }
                             />
-                            <FormQuestion
-                                legend={intlHelper(intl, `introForm.form.${IntroFormField.erArbeidstaker}.spm`)}
-                                name={IntroFormField.erArbeidstaker}
-                                validate={validateYesOrNoIsAnswered}
-                                showInfo={erArbeidstaker === YesOrNo.NO}
-                                infoMessage={intlHelper(intl, 'introForm.form.erArbeidstaker.nei.info')}
-                            />
+                            {legeerklæringBesvart && (
+                                <FormQuestion
+                                    legend={intlHelper(intl, `introForm.form.${IntroFormField.erArbeidstaker}.spm`)}
+                                    name={IntroFormField.erArbeidstaker}
+                                    validate={validateYesOrNoIsAnswered}
+                                    showInfo={erArbeidstaker !== YesOrNo.UNANSWERED}
+                                    infoMessage={
+                                        <>
+                                            <Element tag="h3">
+                                                {erArbeidstaker === YesOrNo.YES &&
+                                                    intlHelper(intl, 'introForm.form.erArbeidstaker.ja.tittel')}
+                                                {erArbeidstaker === YesOrNo.NO &&
+                                                    intlHelper(intl, 'introForm.form.erArbeidstaker.nei.tittel')}
+                                            </Element>
+                                            <p>
+                                                {erArbeidstaker === YesOrNo.YES &&
+                                                    intlHelper(intl, 'introForm.form.erArbeidstaker.ja.info')}
+                                                {erArbeidstaker === YesOrNo.NO &&
+                                                    intlHelper(intl, 'introForm.form.erArbeidstaker.nei.info')}
+                                            </p>
+                                        </>
+                                    }
+                                />
+                            )}
                         </IntroFormComponents.Form>
                     </section>
                 );
