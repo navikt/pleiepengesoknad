8bf799e70ad76ca96d6257c5936a726d0a3f3da1
Frode Hansen <frode.hansen@bekk.no>
2021-05-28 14:39:08 +0200

    Oppdatere pakker

--- jest/setup.ts
+++ jest/setup.ts
@@ -1,6 +1,4 @@
 import Enzyme from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
-import defineFunctionsMissingInJsdom from './defineFunctionsMissingInJsdom';
 
 Enzyme.configure({ adapter: new Adapter() });
-defineFunctionsMissingInJsdom();
