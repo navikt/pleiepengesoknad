122e5c1423c2d926210efd0a51ab98a9e56416c0
Aleksei Poroshin <aleksei.poroshin@nav.no>
2021-05-31 11:21:10 +0200

    Endret "isEndDateInPeriod"

--- src/app/utils/__tests__/frilanserUtils.test.ts
+++ src/app/utils/__tests__/frilanserUtils.test.ts
@@ -4,6 +4,15 @@ describe('Frilanser sluttdato er innenfor søknadsperiode', () => {
     it('should return false if frilans end date is before application period ', () => {
         expect(isEndDateInPeriod('2021-01-31', '2021-01-19')).toBeFalsy();
         expect(isEndDateInPeriod('2021-01-31', '2021-01-30')).toBeFalsy();
+        expect(isEndDateInPeriod('2021-01-31', '2020-01-19')).toBeFalsy();
+        expect(isEndDateInPeriod('2021-01-31', '2020-01-31')).toBeFalsy();
+    });
+
+    it('should return true if frilans end date is in or after application period ', () => {
+        expect(isEndDateInPeriod('2021-01-31', '2021-01-31')).toBeTruthy();
+        expect(isEndDateInPeriod('2021-01-31', '2021-02-01')).toBeTruthy();
+        expect(isEndDateInPeriod('2021-01-31', '2021-02-19')).toBeTruthy();
+        expect(isEndDateInPeriod('2021-01-31', '2022-01-31')).toBeTruthy();
     });
 
     it('should return false if frilans end date is undefined', () => {
@@ -11,12 +20,24 @@ describe('Frilanser sluttdato er innenfor søknadsperiode', () => {
         expect(isEndDateInPeriod('2021-01-31')).toBeFalsy();
     });
 
-    it('should return false if periodeFra og frilans end date is undefined', () => {
+    it('should return false if periodeFra is undefined', () => {
+        expect(isEndDateInPeriod(undefined, '2021-01-31')).toBeFalsy();
+    });
+
+    it('should return false if periodeFra and frilans end date is undefined', () => {
         expect(isEndDateInPeriod(undefined, undefined)).toBeFalsy();
     });
 
-    it('should return true if frilans end date is in eller after application period ', () => {
-        expect(isEndDateInPeriod('2021-01-31', '2021-01-31')).toBeTruthy();
-        expect(isEndDateInPeriod('2021-01-31', '2021-02-19')).toBeTruthy();
+    it('should return false if periodeFra and (or) frilans end date has bad format', () => {
+        expect(isEndDateInPeriod('45gdg89', '2021-01-31')).toBeFalsy();
+        expect(isEndDateInPeriod('2021-32-01', '2021-01-31')).toBeFalsy();
+        expect(isEndDateInPeriod('2021-01-32', '2021-01-31')).toBeFalsy();
+        expect(isEndDateInPeriod('2021-01-31', '2021-01-32')).toBeFalsy();
+        expect(isEndDateInPeriod('2021-01-31', '2021-dr-44')).toBeFalsy();
+        expect(isEndDateInPeriod('2021-01-32', '2021-01-31')).toBeFalsy();
+    });
+
+    it('should return false if periodeFra has bad format', () => {
+        expect(isEndDateInPeriod('undefined23', '344undefined')).toBeFalsy();
     });
 });
--- src/app/utils/frilanserUtils.ts
+++ src/app/utils/frilanserUtils.ts
@@ -1,10 +1,14 @@
-import moment from 'moment';
-moment().format();
+import dayjs from 'dayjs';
+import isSameOrAfter from 'dayjs/plugin/isSameOrAfter';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
 
 export const isEndDateInPeriod = (periodeFra?: string, frilans_slutdato?: string): boolean => {
+    dayjs.extend(isSameOrAfter);
+    dayjs.extend(customParseFormat);
+
     if (periodeFra && frilans_slutdato) {
-        return moment(periodeFra).isValid() && moment(frilans_slutdato).isValid()
-            ? moment(frilans_slutdato) >= moment(periodeFra)
+        return dayjs(periodeFra, 'YYYY-MM-DD', true).isValid() && dayjs(frilans_slutdato, 'YYYY-MM-DD', true).isValid()
+            ? dayjs(frilans_slutdato).isSameOrAfter(periodeFra, 'day')
             : false;
     }
     return false;
