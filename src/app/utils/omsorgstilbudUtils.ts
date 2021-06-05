0e55a380839c891d1579720123ec3ffc3d3c749e
Frode Hansen <frode.hansen@bekk.no>
2021-06-05 06:26:12 +0200

    Ikke spørre månedvis når innenfor 1 måned

--- src/app/utils/omsorgstilbudUtils.ts
+++ src/app/utils/omsorgstilbudUtils.ts
@@ -2,4 +2,5 @@ import { DateRange } from '@navikt/sif-common-formik/lib';
 import dayjs from 'dayjs';
 
 export const skalSpørreOmOmsorgstilbudPerMåned = (søknadsperiode: DateRange): boolean =>
+    dayjs(søknadsperiode.from).isSame(søknadsperiode.to, 'month') === false &&
     dayjs(søknadsperiode.to).diff(søknadsperiode.from, 'days') > 20;
