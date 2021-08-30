31166f9753cd7f055cb74d55b655e29363dc1a2c
Frode Hansen <frode.hansen@bekk.no>
2021-08-30 10:24:03 +0200

    Tekstendringer

--- src/app/components/omsorgstilbud/omsorgstilbudMessages.ts
+++ src/app/components/omsorgstilbud/omsorgstilbudMessages.ts
@@ -15,7 +15,7 @@ const omsorgstilbudMessages = {
         'omsorgstilbud.endreTid': 'Endre tid',
         'omsorgstilbud.form.tittel': 'Omsorgstilbud - {måned}',
         'omsorgstilbud.form.intro.1':
-            'Fyll ut antall timer og minutter de dagene barnet skal være i omsorgstilbudet. Du trenger ikke fylle ut noe de dagene barnet ikke skal være i omsorgstilbud.',
+            'Fyll ut antall timer og minutter de dagene barnet skal være i omsorgstilbudet. Du skal bare fylle ut den tiden du vet med sikkerhet. Du trenger ikke fylle ut noe de dagene barnet ikke skal være i omsorgstilbud.',
         'omsorgstilbud.form.intro_fortid.1':
             'Fyll ut antall timer og minutter de dagene barnet var i omsorgstilbudet. Du trenger ikke fylle ut noe de dagene barnet ikke var i omsorgstilbud.',
         'omsorgstilbud.form.intro.2': 'Du kan registrere opp til 7 timer og 30 minutter per dag.',
