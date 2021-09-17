468294a8d99229ed7117474597ce83aef6eeaec8
Frode Hansen <frode.hansen@bekk.no>
2021-09-17 08:32:55 +0200

    devserver config update

--- src/build/webpack/devserver.config.js
+++ src/build/webpack/devserver.config.js
@@ -1,17 +1,19 @@
+const path = require('path');
 require('dotenv').config();
+
 const mustacheExpress = require('mustache-express');
 const envSettings = require('../../../envSettings');
 
 const configureDevServer = (decoratorFragments) => ({
-    before: (app) => {
-        app.engine('html', mustacheExpress());
-        app.set('views', `${__dirname}/../../../dist/dev`);
-        app.set('view engine', 'mustache');
-        app.get(`${process.env.PUBLIC_PATH}/dist/settings.js`, (req, res) => {
+    onBeforeSetupMiddleware: (devServer) => {
+        devServer.app.engine('html', mustacheExpress());
+        devServer.app.set('views', `${__dirname}/../../../dist/dev`);
+        devServer.app.set('view engine', 'mustache');
+        devServer.app.get(`${process.env.PUBLIC_PATH}/dist/settings.js`, (req, res) => {
             res.set('content-type', 'application/javascript');
             res.send(`${envSettings()}`);
         });
-        app.get(`/dist/settings.js`, (req, res) => {
+        devServer.app.get(`/dist/settings.js`, (req, res) => {
             res.set('content-type', 'application/javascript');
             res.send(`${envSettings()}`);
         });
