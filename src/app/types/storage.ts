a8a2f24f56d9d9c658ea4a3e4ef64b673b5e0c35
Frode Hansen <frode.hansen@bekk.no>
2021-11-30 07:00:24 +0100

    Storage

--- src/app/types/storage.ts
+++ src/app/types/storage.ts
@@ -2,7 +2,7 @@ import { StepID } from '../config/stepConfig';
 import { PleiepengesøknadFormData } from './PleiepengesøknadFormData';
 
 /** Persistence */
-export const MELLOMLAGRING_VERSION = '7.3';
+export const MELLOMLAGRING_VERSION = '7.4';
 interface StorageMetadata {
     version: string;
     lastStepID?: StepID;
