92bacbe5f627d0ecdae3968235661f8b583d8cfe
Frode Hansen <frode.hansen@bekk.no>
2022-01-21 13:49:06 +0100

    Feilogg oppdatering

--- src/app/utils/formToApiMaps/omsorgstilbudApiData.ts
+++ src/app/utils/formToApiMaps/omsorgstilbudApiData.ts
@@ -87,7 +87,11 @@ export const getOmsorgstilbudApiData = (
             ? mapHistoriskOmsorgstilbudToApiData(omsorgstilbud, søknadsperiode, søknadsdato)
             : undefined;
 
-    if (historisk === undefined && planlagt === undefined) {
+    /** Feilsøke situasjon hvor en tidligere sendte inn undefined for både historisk og planlagt */
+    if (
+        (harBarnVærtIOmsorgstilbud === YesOrNo.YES && historisk === undefined) ||
+        (skalBarnIOmsorgstilbud === YesOrNo.YES && planlagt === undefined)
+    ) {
         const payload = {
             harBarnVærtIOmsorgstilbud,
             skalBarnIOmsorgstilbud,
@@ -97,14 +101,18 @@ export const getOmsorgstilbudApiData = (
             planlagtErDefinert: omsorgstilbud.planlagt !== undefined,
         };
         appSentryLogger.logError('Ugyldig omsorgstilbud informasjon', JSON.stringify(payload));
+    }
+
+    if (historisk || planlagt) {
         return {
-            omsorgstilbud: undefined,
+            omsorgstilbud: {
+                historisk,
+                planlagt,
+            },
         };
     }
+
     return {
-        omsorgstilbud: {
-            historisk,
-            planlagt,
-        },
+        omsorgstilbud: undefined,
     };
 };
