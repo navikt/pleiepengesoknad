aeb6a0db898a0c3f1669d323a8a6bf54235e7415
Frode Hansen <frode.hansen@bekk.no>
2022-05-31 13:56:06 +0200

    Oppdatere cypress-test

--- e2e/cypress/integration-utils/steps/arbeidssituasjon.js
+++ e2e/cypress/integration-utils/steps/arbeidssituasjon.js
@@ -2,6 +2,7 @@ const { getTestElement, selectRadioNo, selectRadioYes, setInputTime } = require(
 
 const fyllUtNormalarbeidstidFasteDager = () => {
     selectRadioNo('jobber-fast-helg');
+    selectRadioNo('jobber-fast-deltid');
     selectRadioYes('like-mange-timer-hver-uke');
     selectRadioYes('er-faste-ukedager');
     setInputTime('tid-faste-ukedager__monday', 5, 0);
