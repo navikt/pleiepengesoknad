9ea2db654bf005cdccd5f4b01838c4b569716989
Frode Hansen <frode.hansen@bekk.no>
2022-09-23 11:18:41 +0200

    Tekst - småoppdrag

--- src/app/søknad/arbeidssituasjon-step/components/frilansoppdrag-liste/FrilansoppdragListe.tsx
+++ src/app/søknad/arbeidssituasjon-step/components/frilansoppdrag-liste/FrilansoppdragListe.tsx
@@ -40,7 +40,7 @@ const FrilansoppdragListe: React.FunctionComponent<Props> = ({ frilansoppdrag, k
         <ul style={{ margin: 0, padding: '1rem 0 0 1rem' }}>
             {frilansoppdrag.map((oppdrag) => (
                 <li key={oppdrag.id}>
-                    <Element tag="h4">{oppdrag.navn}</Element>
+                    <Element tag="h3">{oppdrag.navn}</Element>
                     <FormattedMessage id="frilansoppdragListe.oppdrag" values={{ tidsrom: renderTidsrom(oppdrag) }} />
                 </li>
             ))}
