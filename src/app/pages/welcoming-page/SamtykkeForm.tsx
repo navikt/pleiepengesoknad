73fa48842ed82e6e6a63b8a42340a7893a6c8d9e
Aleksei Poroshin <aleksei.poroshin@nav.no>
2022-10-03 18:04:00 +0200

    test

--- src/app/pages/welcoming-page/SamtykkeForm.tsx
+++ src/app/pages/welcoming-page/SamtykkeForm.tsx
@@ -34,6 +34,7 @@ const getBarnNavn = (barn: RegistrerteBarn): string => {
 
 const SamtykkeForm = ({ forrigeSøknad, onConfirm, onOpenDinePlikterModal }: Props) => {
     const intl = useIntl();
+    console.log('forrigeSøknad: ', forrigeSøknad);
     return (
         <AppForm.Form
             onValidSubmit={onConfirm}
