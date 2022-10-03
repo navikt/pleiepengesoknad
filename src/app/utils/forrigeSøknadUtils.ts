d5456d9d6ad71d16888d88e73c1125f25060ac6e
Aleksei Poroshin <aleksei.poroshin@nav.no>
2022-10-03 18:23:45 +0200

    test 2

--- src/app/utils/forrigeSøknadUtils.ts
+++ src/app/utils/forrigeSøknadUtils.ts
@@ -4,10 +4,13 @@ import { InnsendtSøknadInnhold } from '../types/InnsendtSøknad';
 
 export const forrigeSøknadErGyldig = (søknad: InnsendtSøknadInnhold): boolean => {
     if (dayjs(søknad.mottatt).isBefore(ISODateToDate('2022-06-01'), 'day')) {
+        console.log('søknad.mottatt before 2022-06-01');
         return false;
     }
     const maxAlderDato = dayjs().subtract(10, 'weeks'); /** TODO - grense må vurderes */
     if (dayjs(søknad.mottatt).isBefore(maxAlderDato, 'day')) {
+        console.log('søknad.mottatt before maxAlderDato');
+        console.log('maxAlderDato: ', maxAlderDato);
         return false;
     }
     return true;
