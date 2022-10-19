7e550894722922609d2d8eb06888158355407505
Aleksei Poroshin <aleksei.poroshin@nav.no>
2022-10-19 15:30:02 +0200

    Endrett mellomlagring

--- src/app/types/SøknadTempStorageData.ts
+++ src/app/types/SøknadTempStorageData.ts
@@ -1,7 +1,7 @@
 import { StepID } from '../søknad/søknadStepsConfig';
 import { SøknadFormData } from './SøknadFormData';
 
-export const MELLOMLAGRING_VERSION = '12.0.0';
+export const MELLOMLAGRING_VERSION = '12.0.1';
 
 interface StorageMetadata {
     version: string;
