3fd357517fabd22f4f0f0604f363be966150688e
Aleksei Poroshin <aleksei.poroshin@nav.no>
2023-02-22 13:08:48 +0100

    test test

--- server.js
+++ server.js
@@ -66,6 +66,12 @@ const isExpiredOrNotAuthorized = (token) => {
 };
 
 const getRouterConfig = async (req, audienceInnsyn) => {
+    req.headers['X-Correlation-ID'] = uuidv4();
+
+    if (process.env.NAIS_CLIENT_ID !== undefined) {
+        req.headers['X-K9-Brukerdialog'] = process.env.NAIS_CLIENT_ID;
+    }
+
     const token = req.headers['authorization']?.replace('Bearer ', '');
     const cookie = req.cookies['selvbetjening-idtoken'];
 
