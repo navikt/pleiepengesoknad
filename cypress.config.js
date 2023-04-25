ffc1579c8a278da7fefdc8703d1b5d2179e39e9e
Aleksei Poroshin <aleksei.poroshin@nav.no>
2023-04-25 22:36:05 +0200

    fix

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
     e2e: {
-        testIsolation: 'legacy',
+        testIsolation: false,
         // We've imported your old cypress plugins here.
         // You may want to clean this up later by importing these.
         setupNodeEvents(on, config) {
